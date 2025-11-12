6461d1cd203e783ae146cba54f9b80885f01c7be
Dimitri Bourreau <contact@dimitribourreau.me>
2025-11-12 04:41:31 +0100

    feat: set input value to a state

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [inputValue, setInputValue] = useState<string | null>(null);
+
   return (
-    <input
-      placeholder="Ajouter un mot"
-      className="bg-white/20 px-4 py-2 rounded text-center hover:bg-white/50 hover:cursor-pointer"
-    />
+    <section className="flex flex-col gap-4 items-center">
+      <p>{inputValue}</p>
+      <input
+        placeholder="Ajouter un mot"
+        className="bg-white/20 px-4 py-2 rounded text-center hover:bg-white/50 hover:cursor-pointer"
+        onChange={({ target }) => {
+          setInputValue(target.value);
+        }}
+      />
+    </section>
   );
 }
