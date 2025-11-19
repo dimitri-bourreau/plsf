dfc25dcbd1b9ad0485b51075e449dadaef2b3f6d
Dimitri Bourreau <contact@dimitribourreau.me>
2025-11-19 14:51:00 +0100

    feat: when calling elix for video data, use useTransition

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,24 @@
 
 import { WordInput } from "@/components/word-input.component";
 import { SearchResults } from "@/components/search-results.component";
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import { VideoData } from "@/features/video/types/video-data.type";
 import { searchWord } from "@/actions/search-word.action";
 
 export default function Home() {
   const [videoData, setVideoData] = useState<VideoData[]>([]);
+  const [isPending, startTransition] = useTransition();
 
   const handleSearch = async (word: string): Promise<void> => {
-    const searchResults = await searchWord(word);
-    return void setVideoData(searchResults);
+    startTransition(async () => {
+      const searchResults = await searchWord(word);
+      setVideoData(searchResults);
+    });
   };
 
   return (
     <section className="flex flex-col gap-4 items-center">
-      {videoData && <SearchResults results={videoData} />}
-
+      <SearchResults results={videoData} isPending={isPending} />
       <WordInput searchForWord={handleSearch} />
     </section>
   );
--- components/search-results.component.tsx
+++ components/search-results.component.tsx
@@ -1,14 +1,25 @@
 import { VideoData } from "@/features/video/types/video-data.type";
+import clsx from "clsx";
 
 interface SearchResultsProps {
   results: VideoData[];
+  isPending: boolean;
 }
 
-export const SearchResults = ({ results }: SearchResultsProps) => {
+export const SearchResults = ({ results, isPending }: SearchResultsProps) => {
+  if (results.length === 0) {
+    return <></>;
+  }
+
   return (
     <section>
       {results.map((videoData) => (
-        <p key={videoData.id}>{videoData.word}</p>
+        <p
+          key={`${videoData.id}-${videoData.word}`}
+          className={clsx(isPending && "bg-red-500")}
+        >
+          {videoData.word}
+        </p>
       ))}
     </section>
   );
