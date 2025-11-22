ae6efdd1b2cd033920bf2d548cf15e4632366849
Dimitri Bourreau <contact@dimitribourreau.me>
2025-11-22 19:27:44 +0100

    feat: UI is OK for desktops

--- components/result-container.component.tsx
+++ components/result-container.component.tsx
@@ -3,21 +3,16 @@ import clsx from "clsx";
 
 interface ResultsContainerProps {
   children?: ReactNode;
-  isPending?: boolean;
   inputIsEmpty: boolean;
 }
 
 export const ResultsContainer = ({
   children,
   inputIsEmpty,
-  isPending = false,
 }: ResultsContainerProps) => (
   <section
     className={clsx(
-      "p-2 w-full flex flex-col justify-between items-center bg-white/40 rounded transition-all overflow-auto",
-      {
-        "animate-pulse": isPending,
-      },
+      "p-2 w-full flex flex-col justify-between items-center transition-all overflow-auto",
       {
         "opacity-0": inputIsEmpty,
         "opacity-100": !inputIsEmpty,
