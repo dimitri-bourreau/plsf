6ea178530ac9cb4d99376ffc3e923c6358260941
Dimitri Bourreau <contact@dimitribourreau.me>
2025-11-24 00:44:07 +0100

    fix: no sign? careful not to crash! filtering response signs resulted to an empty array

--- features/video/services/get-sign-from-word.service.ts
+++ features/video/services/get-sign-from-word.service.ts
@@ -15,14 +15,15 @@ export const getSignFromWord = async (
   const elixResponse: ElixResponse = await adapter.getVideosForWord(inputQuery);
   if (elixResponse.total === 0) return undefined;
   const sign = getMoreRecentSignVideo(elixResponse.data);
+  if (!sign) return undefined;
   const mappedSignData = mapObject(sign);
   return hasVideoLink(mappedSignData) ? mappedSignData : undefined;
 };
 
 function getMoreRecentSignVideo(
   data: ElixResponse["data"],
-): DefinitionOrSignBase {
-  return data.reverse().filter(removeDefinitions)[0].signs[0];
+): DefinitionOrSignBase | undefined {
+  return data.reverse().filter(removeDefinitions)[0]?.signs[0];
 }
 
 function hasVideoLink(video: signMaybeWithoutVideo): video is Sign {
